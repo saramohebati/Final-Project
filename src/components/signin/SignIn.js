3d2f430f3cc1e87a9c59174e1a1b924a2e855eaf
sara <sara.mohebati71@gmail.com>
2022-08-04 10:16:46 +0430

    Handle error

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,30 +1,47 @@
 import Login from "../../image/Login.png";
 import "./SignIn.css";
-import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Router } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
-  const [userName, setUserName] = useState("");
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passError, setPassError] = useState("");
 
   const login = () => {
+    if (username === "" || password === "") {
+      setUsernameError("");
+      setPassError("");
+      return;
+    }
     axios
-      .post("http://localhost:3000/login", {
-        username: userName,
+      .post("http://localhost:3001/login", {
+        username: username,
         password: password,
       })
       .then((response) => {
-        const token = response.token;
+        const token = response.data.token;
         localStorage.setItem("token", token);
-        Router.push("/poll");
+        setUsername("");
+        setPassword("");
+        navigate("/PollListPage");
       })
-      .catch((error) => {
-        error("Your username or password are not correct!");
+      .catch((res) => {
+        const error = res.response.data;
+        const statuse = res.response.statuse;
+        if (statuse === 404) {
+          setUsernameError(error);
+          setPassError("");
+        } else if (statuse === 403) {
+          setPassError(error);
+          setUsernameError("");
+        }
       });
   };
 
@@ -46,10 +63,10 @@ function SignIn() {
             id="outlined-required"
             label="Username"
             helperText="Please enter your username"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
-
+          <p>{usernameError}</p>
           <TextField
             id="outlined-password-input"
             label="Password"
@@ -59,6 +76,7 @@ function SignIn() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <p>{passError}</p>
           <Button onClick={login} variant="outlined">
             Login
           </Button>
