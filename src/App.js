2fc4a9fd6eee273592c77bf386b1513d10e2f84e
sara <sara.mohebati71@gmail.com>
2022-08-05 23:19:58 +0430

    Changing the signin address

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import Header from "./components/header/Header";
 import LandingPage from "./components/landing page/LandingPage";
-import SignIn from "./components/SignIn";
-import './App.css';
+import SignIn from "./components/signin/SignIn";
+import PollListPage from "./components/poll list page/PollListPage";
+import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-
       <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/PollListPage" element={<PollListPage />} />
+
       </Routes>
 
       <footer>Have Good Day.</footer>
-
     </Router>
   );
 }
